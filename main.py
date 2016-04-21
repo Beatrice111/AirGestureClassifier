d041984f64aa67adc31ed2045a3e3ea22d95b6d6
Chongyuan Xiang <xiangcy@mit.edu>
2016-04-21 18:15:31 -0400

    preprocess all the data for training

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+N_users = 10
+
+# load all the data
+for i in range(N_users):
+	execfile("data/user" + str(i + 1) + ".py")
+
+# preprocessing
+all_data = []
+all_labels = []
+for D in [data_user1, data_user2, data_user3, data_user4, data_user5, \
+	data_user6, data_user7, data_user8, data_user9, data_user10]:
+	for label, d in D:
+		d = np.array(d)
+		# only extract ax, ay out of the signals
+		d = d[:, 7:9]
+		# scale the data to be in range [-1,1]
+		d = d / np.amax(np.abs(d), axis = 0)
+		all_data.append(d)
+		all_labels.append(label)
+
+# separate into training data and testing data
+training_x = all_data[:900]
+training_y = all_labels[:900]
+testing_x = all_data[900:]
+testing_y = all_labels[900:]
